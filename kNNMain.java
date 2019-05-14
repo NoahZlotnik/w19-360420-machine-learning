da6ec0e61c3e43472d6676d2668557418f3b2a2c
Jon Zlotnik <j.zlotnik@live.ca>
2019-05-14 15:58:13 -0400

    We are done now

--- kNNMain.java
+++ kNNMain.java
@@ -3,53 +3,126 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 
+
 public class kNNMain{
 
-  public static void main(String[] args) throws FileNotFoundException{
+  public static void main(String... args) throws FileNotFoundException{
 
-    // TASK 1: Use command line arguments to point DataSet.readDataSet method to
-    // the desired file. Choose a given DataPoint, and print its features and label
+   
+	String Path_To_Data = args[0];
+	DataSet Iris = new DataSet();
+	List<DataPoint> Iris1 = DataSet.readDataSet(Path_To_Data);
 	
-	String PATH_TO_DATA = args[0]; 										// gets first cmd line arg
-	List<DataPoint> irisData = DataSet.readDataSet(PATH_TO_DATA); 	// passes DataSet.readDataSet a file path and stores return value
-	DataPoint chosenDataPoint = irisData.get(87);						// stores a given datapoint (the first one)
-	//System.out.println("Features:");
-	for (int i = 0; i < chosenDataPoint.x.length; i++) { 				// iterates through features of datapoint's 'x' attribute and prints them
-		//System.out.println(chosenDataPoint.x[i]);
+	DataPoint a = Iris1.get(5);
+	String Label = a.getLabel();
+	double [] values = a.getX();
+	
+	/*System.out.println( Label);
+	
+	for(int i=0; i< values.length; i ++)
+	{
+		System.out.println( + values[i]);
+	}*/
+	
+	
+	
+	KNNClassifier thingToFind = new KNNClassifier (17);
+	
+	
+	
+	
+	/*DataPoint IrisA = Iris1.get(0);
+	double [] valuesA = IrisA.getX();
+	for(int i=1; i< values.length; i ++)
+	{
+		System.out.println( + valuesA[i]);
 	}
-	//System.out.println("Label: " + chosenDataPoint.label);				// prints datapoint's label attribute
-
-
-    //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
-	List<DataPoint> training = DataSet.getTrainingSet(irisData,0.8);
-	List<DataPoint> testing	 = DataSet.getTestSet(irisData,0.2);
-
-
-
-    // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
-    // and returns the Euclidean distance between those two points (as a double)
-	// all done
-
-
-    // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
-    // and make a print a predicted target label
-	KNNClassifier figureOutThisBoi = new KNNClassifier(5);
-	//String gotty = figureOutThisBoi.predict(irisData,chosenDataPoint);
-	//System.out.println(gotty);
-
-		String gotty = figureOutThisBoi.predict(training,testing.get(0));
-		int count = 0;
-    // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
-    // point based on nearest neighbors in training set. Calculate accuracy of model.
-	for(int i = 0; i<testing.size();i++){
-		gotty = figureOutThisBoi.predict(training,testing.get(i));
-		//System.out.println(gotty);
-		//System.out.println(testing.get(i).getLabel());
-		if(gotty.equals(testing.get(i).getLabel())){
-			count++;
-		}	
+	DataPoint IrisB = Iris1.get(40);
+	double [] valuesB = IrisB.getX();
+	for(int i=1; i< values.length; i ++)
+	{
+		System.out.println( + valuesB[i]);
 	}
-	System.out.println("Accuracy is " + (double)(count) / (double)(testing.size()) * 100. + "%");	
+	double distance = DataSet.distanceEuclid(IrisA,IrisB);
+	System.out.println(distance);*/
+
+	double [] storage = new double [1000];
+	double percentTotal = 0;
+	int ctr = 0;
+	int ctr2 = 0;
+	int ctr3 = 0;
+	double precision = 0;
+	int ctr4 = 0;
+	int ctr5 = 0;
+	double recall = 0;
+	
+	
+	for (int j = 0; j < 1000; j ++)
+	{
+		Iris1 = DataSet.readDataSet(Path_To_Data);
+		List<DataPoint> Insurance = DataSet.getTestSet(Iris1, 0.3);
+		List<DataPoint> Train = DataSet.getTrainingSet(Iris1, 0.7);
+		
+		ctr = 0;
+		for (int i = 0; i < Insurance.size(); i ++)
+		{
+			String predictedThingy = thingToFind.predict( Train, Insurance.get(i));
+			if(predictedThingy .equals (Insurance.get(i).getLabel()))
+			{
+				ctr++; 
+			}
+			//System.out.println( predictedThingy);
+		}
+		double percentage = (double)(ctr)/(double)(Insurance.size()) * 100;
+		storage[j] = percentage;
+		percentTotal = percentTotal + percentage;
+		ctr2 = 0;
+		ctr3 = 0;
+		for(int i = 0; i < Insurance.size(); i ++)
+		{
+			String predictedThingy = thingToFind.predict(Train,Insurance.get(i));
+			if(predictedThingy.equals("malignant"))
+			{
+				ctr3++;
+				if (predictedThingy.equals(Insurance.get(i).getLabel()))
+				{
+					ctr2++;
+				}
+			}
+			
+			
+		}
+		
+		precision = precision + ((double)(ctr2))/((double)(ctr3))*100.0;
+		ctr4 = 0;
+		ctr5 = 0;
+		for(int i = 0; i < Insurance.size(); i++)
+		{
+			String predictedThingy = thingToFind.predict(Train,Insurance.get(i));
+			if(Insurance.get(i).getLabel().equals("malignant"))
+			{
+				ctr5++;
+				if(predictedThingy.equals(Insurance.get(i).getLabel()))
+				{
+					ctr4++;
+				}
+			}
+		}
+		recall = recall + ((double)(ctr4))/((double)(ctr5))*100.0;
+	}
+	
+	System.out.println("Recall is " + recall/1000.0 + "%");
+	System.out.println("Precision is " + precision/1000.0 + "%");
+//	double finalPercent = percentTotal/1000.0;
+//	System.out.println(finalPercent + " average");
+	double average = mean(storage);
+	System.out.println(average + " mean");
+	double sd = standardDeviation(storage);
+	System.out.println(sd + " standard deviation");
+	DataSet.printLabelFrequencies(Iris1);
+	
+
+	
   }
 
   public static double mean(double[] arr){
